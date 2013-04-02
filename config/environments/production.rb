c8313338968d5133130fdd7ca28bb7bb432dd135
Marcella Roney <marcella.roney@gmail.com>
2013-04-02 00:07:25 -0400

    Connect Amazon S3 Securely

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -70,9 +70,9 @@ Omrails::Application.configure do
   config.paperclip_defaults = {
     :storage => :s3,
     :s3_credentials => {
-      :bucket => "onemonthrailssavedbythe90s",
-      :access_key_id => "AKIAJ25XGX7MVJ2VYL2A",
-      :secret_access_key => "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"
+      :bucket => ENV['AWS_BUCKET'],
+      :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
     }
   }
 end
