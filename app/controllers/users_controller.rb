52bb92c8ee715c50607c71db65e4d0d426d95e0d
Marcella Roney <marcella.roney@gmail.com>
2013-04-10 00:24:30 -0400

    Fix User Page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,6 @@
 class UsersController < ApplicationController
   def show
   	@user = User.find(params[:id])
-  	@pins = @user.pins.page(params[:page]).per_page(30)
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.json { render json: @pins }
-      format.js
+  	@pins = @user.pins
   end
 end
