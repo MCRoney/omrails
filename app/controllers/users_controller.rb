8ffab83e7af51b5da506fe663b09309ce0cb82f2
Marcella Roney <marcella.roney@gmail.com>
2013-04-10 00:18:08 -0400

    Add Pagination to users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,9 @@ class UsersController < ApplicationController
   	@user = User.find(params[:id])
   	@pins = @user.pins.page(params[:page]).per_page(30)
 
-  	respond_to do |format|
+    respond_to do |format|
+      format.html # index.html.erb
+      format.json { render json: @pins }
       format.js
   end
 end
