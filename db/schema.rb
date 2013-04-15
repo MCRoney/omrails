9875e733a98dd24175933b81bf8e4b2785bf6f12
Marcella Roney <marcella.roney@gmail.com>
2013-04-15 00:09:26 -0400

    Add reputations

--- db/schema.rb
+++ db/schema.rb
@@ -27,7 +27,7 @@ ActiveRecord::Schema.define(:version => 20130413195830) do
 
   add_index "pins", ["user_id"], :name => "index_pins_on_user_id"
 
-  create_table "rs_evaluations", :force => true do |t|
+ create_table "rs_evaluations", :force => true do |t|
     t.string   "reputation_name"
     t.integer  "source_id"
     t.string   "source_type"
@@ -70,24 +70,4 @@ ActiveRecord::Schema.define(:version => 20130413195830) do
   add_index "rs_reputations", ["reputation_name", "target_id", "target_type"], :name => "index_rs_reputations_on_reputation_name_and_target", :unique => true
   add_index "rs_reputations", ["reputation_name"], :name => "index_rs_reputations_on_reputation_name"
   add_index "rs_reputations", ["target_id", "target_type"], :name => "index_rs_reputations_on_target_id_and_target_type"
-
-  create_table "users", :force => true do |t|
-    t.string   "email",                  :default => "", :null => false
-    t.string   "encrypted_password",     :default => "", :null => false
-    t.string   "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          :default => 0
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip"
-    t.string   "last_sign_in_ip"
-    t.datetime "created_at",                             :null => false
-    t.datetime "updated_at",                             :null => false
-    t.string   "name"
-  end
-
-  add_index "users", ["email"], :name => "index_users_on_email", :unique => true
-  add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
-
 end
