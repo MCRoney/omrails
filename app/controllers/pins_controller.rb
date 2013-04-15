23f26b64b168ede3e4e4cfc4cd11676603ddc25d
Marcella Roney <marcella.roney@gmail.com>
2013-04-15 00:02:56 -0400

    Add Voting Function

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -7,7 +7,7 @@ def index
   if params[:search]
     @pins = Pin.page(params[:page]).per_page(30).find(:all, :conditions => ['description LIKE ?', "%#{params[:search]}%"])
   else  
-    @pins = Pin.order("created_at desc").page(params[:page]).per_page(30)
+    @pins = Pin.page(params[:page]).per_page(30).find_with_reputation(:votes, :all, order: "created_at desc")
 
     respond_to do |format|
       format.html # index.html.erb
