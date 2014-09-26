0ac914227c477d51deeacb2dbda0f3676e01a06c
Marcella Roney <marcella.roney@gmail.com>
2014-09-25 21:46:41 -0400

    restore all images

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -41,7 +41,7 @@ end
 
   # GET /pins/1/edit
   def edit
-    @pin = current_user.pins.find(params[:id])
+    @pin = Pin.find(params[:id])
   end
 
   # POST /pins
@@ -63,7 +63,7 @@ end
   # PUT /pins/1
   # PUT /pins/1.json
   def update
-    @pin = current_user.pins.find(params[:id])
+    @pin = Pin.find(params[:id])
 
     respond_to do |format|
       if @pin.update_attributes(params[:pin])
